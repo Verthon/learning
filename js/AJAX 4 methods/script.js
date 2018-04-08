cce00fa6f6d23d743b4926fec709a9b23dc5db6b
Verthon <krzysiekksordyl@gmail.com>
2018-04-08 11:03:00 +0200

    completed fetch, jquery and axios

--- js/AJAX 4 methods/script.js
+++ js/AJAX 4 methods/script.js
@@ -1,3 +1,7 @@
+//Working Demo https://codepen.io/Verthon/pen/bvQaYO
+
+
+
 const url = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
 const xhrBtn = document.getElementById('xhr'),
       fetchBtn = document.getElementById('fetch'),
@@ -5,7 +9,7 @@ const xhrBtn = document.getElementById('xhr'),
       axiosBtn = document.getElementById('axios'),
       quote = document.getElementById('quote');
 
-//XHR
+//-------------------XHR---------------------------
 
 
 xhrBtn.addEventListener('click', function(){
@@ -19,5 +23,57 @@ xhrBtn.addEventListener('click', function(){
   xhr.open("GET", url);
   xhr.send();
   
-  console.log(xhr.response.status);
+});
+
+//-------------------------fetch---------------------
+
+fetchBtn.addEventListener('click', function(){
+  fetch(url)
+    .then(handleErrors)
+    .then(parseJSON)
+    .then(updateQuote)
+    .catch(printError)
+  
+  function handleErrors(request){
+    if(!request){
+    throw Error(request.status);
+    }
+    return request;
+  }
+  
+  function parseJSON(request){
+    return request.json();
+  }
+  
+  function updateQuote(data){
+    quote.innerHTML = data;
+  }
+  
+  function printError(error){
+    console.log(error)
+  }
+});
+
+//---------------------jquery-------------------------
+
+$(jqueryBtn).click(function(){
+  $.getJSON(url, function(json){
+    quote.innerHTML = json;
+  });
+})
+
+//---------------------axios---------------------------
+
+axiosBtn.addEventListener('click', function(){
+  axios.get(url)
+    .then(updateDom)
+    .catch(errorHandler)
+  
+  function updateDom(data){
+    quote.innerHTML = (data.data);
+  }
+  
+  function errorHandler(error){
+    console.log(error);
+  }
 });
