0aeb9ed1680ca6020a6c0b8f49547e18c5e2dc78
Verthon <krzysiekksordyl@gmail.com>
2018-04-13 17:02:10 +0200

    added array example to constructor in javascript

--- js/object contructor/contructor.js
+++ js/object contructor/contructor.js
@@ -9,3 +9,12 @@ function Course(title, instructor, level, published, views){
 const Course01 = Course('javascript', 'unknown', 0, true, 0);
 
 console.log(Course01);
+
+//same with arrays
+
+const arr = [
+  Course('javascript', 'unknown', 0, true, 0),
+  Course('php', 'unknown1', 1, false, 0)
+]
+
+console.log(arr);
